2343bfb3aa3c4214af0a0f4049fcb438dc9385d9
NehuenCuenca <nehuen.cuenca@hotmail.com>
2021-04-13 16:05:46 -0300

    tp seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(AutoSeeder::class);
-        \App\Models\Moto::factory(10)->create();
+        \App\Models\Moto::factory(99)->create();
     }
 }
