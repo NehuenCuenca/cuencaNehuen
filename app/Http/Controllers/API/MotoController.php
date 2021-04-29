18e80e943aa08f02800c23a3396dea1f9e604741
NehuenCuenca <nehuen.cuenca@hotmail.com>
2021-04-29 16:15:17 -0300

    practica de api de motos

---
+++ app/Http/Controllers/API/MotoController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Moto;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //return Moto::all();
+        
+        $Motos = Moto::all();
+        return $Motos->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/Moto.php
+++ app/Models/Moto.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Moto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'marca',
+        'modelo',
+        'color',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
